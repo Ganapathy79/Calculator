675cd2c05971121c419e395c1337c1e823221d45
R.Joseph Fabio <josephfabio341@gmail.com>
2025-10-27 20:55:51 +0530

    Add configuration file for Sphinx documentation

---
+++ conf.py
@@ -0,0 +1,14 @@
+project = 'Calculator Project'
+author = 'Your Name'
+release = '1.0.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
